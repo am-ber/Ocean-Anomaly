533caab8745ccc4572a3a2bc0dda79ba5e245176
saydoer <charlieden@gmail.com>
2024-08-17 00:29:26 -0400

    Large Monster

--- Ocean-Anomaly/Assets/Scripts/MonsterMovement.cs
+++ Ocean-Anomaly/Assets/Scripts/MonsterMovement.cs
@@ -7,15 +7,26 @@ public class MonsterMovement : MonoBehaviour
     public float timeFast = 0.05f;
     public float timeSlow = 0.01f;
     
-    public float swayTo = -15f;
-    public float swayFro = 15f;
+    public float tailLeftX = -15f;
+    public float tailMidX = -1f;
+    public float tailRightX = 15f;
+    
+    public float tailMinY = 0f;
+    public float tailMaxY = 30f;
+    
+    public float to = -15f;
+    public float fro = 15f;
     
     public float waitCount;
-    public float waitDuration;
+    public float waitDuration = 2.5f;
     
     public Transform target;
     
     static float t = 0.0f;
+    
+    public float largeAmount = 10f;
+    public float medAmount = 1f;
+    public float smallAmount = 0.5f;
     
     float headDirectionPrevious;
     
@@ -23,11 +34,17 @@ public class MonsterMovement : MonoBehaviour
     Vector3 tailMiddle;
     Vector3 tailRight;
     
+    Vector3 swayTo;
+    Vector3 swayFro;
+    
     void Start ()
     {
-        tailLeft = new Vector3(-15f, 30f, 0f);
-        tailMiddle = new Vector3(-1f, 0f, 0f);
-        tailRight = new Vector3(15f, 30f, 0f);
+        tailLeft = new Vector3(tailLeftX, tailMaxY, 0f);
+        tailMiddle = new Vector3(tailMidX, tailMinY, 0f);
+        tailRight = new Vector3(tailRightX, tailMaxY, 0f);
+        
+        swayTo = new Vector3(to, 0f, 0f);
+        swayFro = new Vector3(fro, 0f, 0f);
     }
 
     void Update ()
@@ -52,31 +69,32 @@ public class MonsterMovement : MonoBehaviour
             Debug.Log("1");
             
             target.localPosition = Vector3.Lerp(targetPos, tailLeft, timeFast);
+            
+            waitCount = 0f;
         }
         //Otherwise, if the direction of the head is equal to its previous direction, animate the IK target between its current position and its middle position
         else if (headDirection == headDirectionPrevious)
         {
             Debug.Log("2");
             
-            for ()
-            target.localPosition = Vector3.Lerp(targetPos, tailMiddle, timeSlow);
+            waitCount += Time.deltaTime;
             
-            currentWaitTime += Time.deltaTime;
-            
-            if (currentWaitTime >= waitDuration)
+            if (waitCount < waitDuration)
+                target.localPosition = Vector3.Lerp(targetPos, tailMiddle, timeSlow);
+            else
             {
                 // animate the position of the game object...
-                target.localPosition = new Vector3(Mathf.Lerp(swayTo, swayFro, t), 5f, 0);
+                target.localPosition = Vector3.Lerp(swayTo, swayFro, t);
                 
                 // .. and increase the t interpolater
-                t += 0.5f * Time.deltaTime;
+                t += smallAmount * Time.deltaTime;
                 
                 // now check if the interpolator has reached 1.0
                 // and swap maximum and minimum so game object moves
                 // in the opposite direction.
                 if (t > 1.0f)
                 {
-                    float temp = swayFro;
+                    Vector3 temp = swayFro;
                     swayFro = swayTo;
                     swayTo = temp;
                     t = 0.0f;
@@ -89,6 +107,8 @@ public class MonsterMovement : MonoBehaviour
             Debug.Log("3");
             
             target.localPosition = Vector3.Lerp(targetPos, tailRight, timeFast);
+            
+            waitCount = 0f;
         }
         
         //Record the the previous direction of the head
